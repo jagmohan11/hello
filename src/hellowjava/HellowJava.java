d62a4117248e1d89fecfc404dc6885ac855e1c2d
jagmohan <jagmohan@jagmohan-HP-G42-Notebook-PC>
2018-06-10 13:07:12 +0530

    string stringbuffer

--- src/hellowjava/HellowJava.java
+++ src/hellowjava/HellowJava.java
@@ -16,8 +16,13 @@ public class HellowJava {
      */
     public static void main(String[] args) {
         String s="hello Git from java";
-        
-        System.out.print(s);
+        StringBuffer s1= new StringBuffer("here is variable length string");
+        System.out.println(s);
+        System.out.println(s1);
+        System.out.println(s1.length());
+        String s2= new String("here is a string");
+        System.out.println(s2);
+        System.out.println(s2.length());
         // TODO code application logic here
     }
     
